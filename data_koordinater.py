ee919c9bad8b397cdfb386685082d3228ffc83f9
NoobLoadPwn <mrsmordhorst@gmail.com>
2021-04-20 10:14:59 +0200

    Med kommentarer

--- data_koordinater.py
+++ data_koordinater.py
@@ -4,25 +4,21 @@ import json
 
 
 def findkoordinater(lokation):
-	conn = http.client.HTTPConnection('api.positionstack.com')
+	conn = http.client.HTTPConnection('api.positionstack.com')#definerer hvilken api den skal gå ind
 
-	params = urllib.parse.urlencode({
-		'access_key': '954f1d2dd6ec71f9e71b8ba50194fa7d',
-		'query': lokation,
+	params = urllib.parse.urlencode({#tildeler nødvendige parametre
+		'access_key': '954f1d2dd6ec71f9e71b8ba50194fa7d',#bruger acces kodenfor at få adgang til api
+		'query': lokation,#sætter parameteren til at være det samme som den givede data
 		'limit': 1,
 		})
 
-	conn.request('GET', '/v1/forward?{}'.format(params))
+	conn.request('GET', '/v1/forward?{}'.format(params)) #kalder inputtet fra apien, med de rigtige parametre
+	res = conn.getresponse()#får response (denne del aer mere sikkerhed end nødvendig)
+	data = res.read() #læser reponsen, og dermed får dataen, fra at kalde apien.
+	decodeddata = data.decode('utf-8') #sætter data til rigtig datatype altså string, for at programmet kan læse det.
+	decodeddatadict = json.loads(decodeddata) #bruger json til at lave string om til dictionaries, for at kunne plukke de rigtige dele af data.
 
-	res = conn.getresponse()
+	latitude = decodeddatadict['data'][0]['latitude'] #tager latitude fra decodeddatadict
+	longitude = decodeddatadict['data'][0]['longitude'] #tager longitude fra decodeddatadict
 
-	data = res.read()
-
-	decodeddata = data.decode('utf-8')
-
-	decodeddatadict = json.loads(decodeddata)
-	
-	latitude = decodeddatadict['data'][0]['latitude']
-	longitude = decodeddatadict['data'][0]['longitude']
-
-	return latitude, longitude
+	return latitude, longitude #returnere latitude og longitude
--- data_toilet.py
+++ data_toilet.py
@@ -14,47 +14,46 @@ def page():
 
 def number_of_toilets(data_number_of_toilets):
 	toiletamount='per_page='
-	toiletamount+=data_number_of_toilets
+	toiletamount+=data_number_of_toilets#tiljøer den tildelte data til inket, som skal være et tal
 	return toiletamount
 
 def offset():
 	return 'offset=0'
 
 def ada_accesible(data_ADA):
-	if data_ADA==True:
+	if data_ADA==True:#hvis ada er sand bliver der skrevet en string med true, og hvis ikke bliver der skrevet false
 		return  'ada=true'
 	elif data_ADA==False:
 		return 'ada=false'
 
 def unisex(data_unisex):
-	if data_unisex==True:
+	if data_unisex==True:#hvis unisex er sand bliver der skrevet en string med true, og hvis ikke bliver der skrevet false
 		return 'unisex=true'
 	elif data_unisex==False:
 		return 'unisex=false'
 
 def latitude(data_lokation):
 	latitude='lat='
-	latitude+=str(findkoordinater(data_lokation)[0])
+	latitude+=str(findkoordinater(data_lokation)[0])#kalder findkordinater for  latitude, ud fra det input den modtager, og laver det om til string, for at det i linket
 	return latitude
 
 def longitude(data_lokation):
 	longitude='lng='
-	longitude+=str(findkoordinater(data_lokation)[1])
+	longitude+=str(findkoordinater(data_lokation)[1])#kalder findkordinater for  longitude, ud fra det input den modtager, og laver det om til string, for at det i linket
 	return longitude
 
 def link_constructer(data_number_of_toilets, data_ADA, data_unisex, data_lokation):
-	link='https://www.refugerestrooms.org/api/v1/restrooms/by_location?'
+	link='https://www.refugerestrooms.org/api/v1/restrooms/by_location?'#sætter basis delen af linket, der altid skal være det samme
+	#kalder alle de forskellige funktioner som piecelink, for at kunne indsætte inputet i rigtig form som et link til api'en.
 	for piecelink in [page(), number_of_toilets(data_number_of_toilets),offset(), ada_accesible(data_ADA), unisex(data_unisex), latitude(data_lokation), longitude(data_lokation)]:
-		if piecelink!='':
-			link+='&'+piecelink
+		if piecelink!='':#går igennem de forskellige dele af linket, hvis de ikke er blanke
+			link+='&'+piecelink #sætter de forskellige dele af linket ind i linket, med et & tegn imellem
 	return link
 
-test_link=link_constructer(test_number_of_toilets, test_ADA, test_unisex, test_lokation)
-
 def data_constructer(link):
-	datalink=requests.get(link).text
-	decodeddatadict = json.loads(datalink) #laver
-	addreses=[]
+	datalink=requests.get(link).text #finder linkets input
+	decodeddatadict = json.loads(datalink) #laver linkets
+	addreses=[]#opretter tomme lister
 	directions=[]
 	downvote=[]
 	upvote=[]
@@ -64,7 +63,7 @@ def data_constructer(link):
 	state=[]
 	name=[]
 	for dict in decodeddatadict:#går igennem de forskellige dicts
-		addreses.append(dict['street']) #tager adressen og sætter den ind i adresses listen
+		addreses.append(dict['street']) #tager dataen fra dict og sætter den ind i dens tilgivne liste
 		directions.append(dict['directions'])
 		downvote.append(dict['downvote'])
 		upvote.append(dict['upvote'])
@@ -74,5 +73,3 @@ def data_constructer(link):
 		state.append(dict['state'])
 		name.append(dict['name'])
 	return addreses, directions, downvote, upvote, changing_table, approved, city, state, name
-
-data=data_constructer(test_link)
